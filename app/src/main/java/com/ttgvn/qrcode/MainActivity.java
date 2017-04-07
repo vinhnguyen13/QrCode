3c909eb1b638f801e66645dbd353821be2c9fafb
vinhnguyen13 <quangvinh.nguyen@trungthuygroup.vn>
2017-04-07 18:00:45 +0700

    Check login

--- app/src/main/java/com/ttgvn/qrcode/MainActivity.java
+++ app/src/main/java/com/ttgvn/qrcode/MainActivity.java
@@ -1,7 +1,10 @@
 package com.ttgvn.qrcode;
 
 import android.content.ContentValues;
+import android.content.Context;
+import android.location.Location;
 import android.os.StrictMode;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -10,6 +13,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -19,14 +23,14 @@ import android.widget.Toast;
 import com.auth0.android.jwt.Claim;
 import com.auth0.android.jwt.JWT;
 
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -35,16 +39,23 @@ import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
     static final String ACTION_SCAN = "com.google.zxing.client.android.SCAN";
+    static final String LOG_D = "TTGD";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
-        Log.d("TTGD", "----SDK_INT----:" + android.os.Build.VERSION.SDK_INT);
+        Log.d(LOG_D, "----SDK_INT----:" + android.os.Build.VERSION.SDK_INT);
         if (android.os.Build.VERSION.SDK_INT > 9) {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
         }
+
+        /*TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+        String identifier = getDeviceID(telephonyManager);
+        TextView tv = (TextView)findViewById(R.id.textView1);
+        tv.setText("Hello, "+identifier);*/
+
     }
 
     public void authentication(View v) {
@@ -54,15 +65,20 @@ public class MainActivity extends AppCompatActivity {
 
         EditText txtPhone = (EditText) findViewById(R.id.txtPhone);
         EditText txtPass = (EditText) findViewById(R.id.txtPass);
-        Log.d("TTGD", "Login >> phone:" + txtPhone.getText().toString() + " pass:" + txtPass.getText().toString());
+        Log.d(LOG_D, "Login >> phone:" + txtPhone.getText().toString() + " pass:" + txtPass.getText().toString());
         try {
             // CALL GetText method to make post method call
             String Response = postLogin(txtPhone, txtPass);
-            Log.d("TTGD", "Response:" + Response);
-            setContentView(R.layout.activity_main);
-            TextView tv = (TextView)findViewById(R.id.textUser);
-            tv.setText("Hello, "+txtPhone.getText().toString());
-            Toast.makeText(this, "Login Success" + Response, Toast.LENGTH_LONG).show();
+            Log.d(LOG_D, "Response:" + Response);
+            JSONObject jObject = new JSONObject(Response);
+            if(jObject.getInt("code") == 0){
+                setContentView(R.layout.activity_main);
+                TextView tv = (TextView)findViewById(R.id.textUser);
+                tv.setText("Hello, "+txtPhone.getText().toString());
+                Toast.makeText(this, "Login Success", Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(this, "Login Fail, please check data input again", Toast.LENGTH_LONG).show();
+            }
         } catch (Exception ex) {
             ex.printStackTrace();
             Toast.makeText(this, "URL Exception", Toast.LENGTH_LONG).show();
@@ -79,7 +95,7 @@ public class MainActivity extends AppCompatActivity {
         String Pass = txtPass.getText().toString();
         String data = URLEncoder.encode("phone", "UTF-8") + "=" + URLEncoder.encode(Phone, "UTF-8");
         data += "&" + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(Pass, "UTF-8");
-        Log.d("TTGD", "Data:" + data);
+        Log.d(LOG_D, "Data:" + data);
         try {
             URL url = new URL("http://dev.metvuong.com/api/login");
             // Send POST data request
@@ -95,20 +111,20 @@ public class MainActivity extends AppCompatActivity {
             connection.setUseCaches(false);
             connection.setDoInput(true);
             connection.setDoOutput(true);
-            Log.d("TTGD", "connection:" + connection);
+            Log.d(LOG_D, "connection:" + connection);
             //send request
             DataOutputStream wr = new DataOutputStream(
                     connection.getOutputStream());
-            Log.d("TTGD", "DataOutputStream:" + wr);
+            Log.d(LOG_D, "DataOutputStream:" + wr);
             wr.writeBytes(data);
             wr.flush();
             wr.close();
 
             // Get the server response
             InputStream is = connection.getInputStream();
-            Log.d("TTGD", "InputStream:" + is);
+            Log.d(LOG_D, "InputStream:" + is);
             BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-            Log.d("TTGD", "BufferedReader:" + rd);
+            Log.d(LOG_D, "BufferedReader:" + rd);
             String line;
             StringBuffer response = new StringBuffer();
             while ((line = rd.readLine()) != null) {
@@ -177,17 +193,97 @@ public class MainActivity extends AppCompatActivity {
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
         if (requestCode == 0) {
             if (resultCode == RESULT_OK) {
-                Log.d("TTGD", "-------intent:" + intent);
+                Log.d(LOG_D, "-------intent:" + intent);
                 //get the extras that are returned from the intent
                 String contents = intent.getStringExtra("SCAN_RESULT");
                 String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
-                Toast toast = Toast.makeText(this, "Content:" + contents + " Format:" + format, Toast.LENGTH_LONG);
+                String textResult = "Content:" + contents + " Format:" + format;
+
+                /**
+                 * imei
+                 */
+
+                String identifier = getDeviceID();
+
+                textResult += " identifier: "+identifier;
+                Toast toast = Toast.makeText(this, textResult, Toast.LENGTH_LONG);
                 toast.show();
             }
         }
     }
 
-    public String sendDataAfterScan(){
-        return "";
+    /**
+     * get Imei phone
+     * @return
+     */
+    String getDeviceID(){
+        TelephonyManager phonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+        String id = phonyManager.getDeviceId();
+        if (id == null){
+            id = "not available";
+        }
+        int phoneType = phonyManager.getPhoneType();
+        String phoneTypeText;
+        switch(phoneType){
+            case TelephonyManager.PHONE_TYPE_NONE:
+                phoneTypeText = "NONE: " + id;
+            case TelephonyManager.PHONE_TYPE_GSM:
+                phoneTypeText = "GSM: IMEI=" + id;
+            case TelephonyManager.PHONE_TYPE_CDMA:
+                phoneTypeText = "CDMA: MEID/ESN=" + id;
+            default:
+                phoneTypeText = "UNKNOWN: ID=" + id;
+        }
+        return id;
+    }
+
+    public String sendDataAfterScan(String username, String location, String imei, String qrData) throws UnsupportedEncodingException{
+        HttpURLConnection connection = null;
+        String data = URLEncoder.encode("username", "UTF-8") + "=" + URLEncoder.encode(username, "UTF-8");
+        data += "&" + URLEncoder.encode("location", "UTF-8") + "=" + URLEncoder.encode(location, "UTF-8");
+        data += "&" + URLEncoder.encode("imei", "UTF-8") + "=" + URLEncoder.encode(imei, "UTF-8");
+        data += "&" + URLEncoder.encode("qrData", "UTF-8") + "=" + URLEncoder.encode(qrData, "UTF-8");
+        Log.d(LOG_D, "Data:" + data);
+        try {
+            URL url = new URL("http://dev.metvuong.com/api/login");
+            // Send POST data request
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setReadTimeout(10000);
+            connection.setConnectTimeout(15000);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type",
+                    "application/x-www-form-urlencoded");
+            connection.setRequestProperty("Content-Length", "" +
+                    Integer.toString(data.getBytes().length));
+            connection.setRequestProperty("Content-Language", "en-US");
+            connection.setUseCaches(false);
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            Log.d(LOG_D, "connection:" + connection);
+            //send request
+            DataOutputStream wr = new DataOutputStream(
+                    connection.getOutputStream());
+            Log.d(LOG_D, "DataOutputStream:" + wr);
+            wr.writeBytes(data);
+            wr.flush();
+            wr.close();
+
+            // Get the server response
+            InputStream is = connection.getInputStream();
+            Log.d(LOG_D, "InputStream:" + is);
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+            Log.d(LOG_D, "BufferedReader:" + rd);
+            String line;
+            StringBuffer response = new StringBuffer();
+            while ((line = rd.readLine()) != null) {
+                response.append(line);
+                response.append('\r');
+            }
+            rd.close();
+            return response.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
